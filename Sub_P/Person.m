75783df8e7035218b3249d5cd405b4707f9b54e6
永平 <yongping@anve.com>
2020-04-01 16:21:44 +0800

    init

---
+++ Sub_P/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Sub_P
+//
+//  Created by 永平 on 2020/4/1.
+//  Copyright © 2020 yongping. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
---
+++ Sub_P/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sub_P
+//
+//  Created by 永平 on 2020/4/1.
+//  Copyright © 2020 yongping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
